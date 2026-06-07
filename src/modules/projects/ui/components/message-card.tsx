081af3ad8701357eaa87bf7e367a96ab5c452747
Yusup Maulana <yusupmaulana950@gmail.com>
2026-06-07 20:38:08 +0700

    fix: correct 'rounded-g' typo to 'rounded-lg' in UserMessage className

--- src/modules/projects/ui/components/message-card.tsx
+++ src/modules/projects/ui/components/message-card.tsx
@@ -13,7 +13,7 @@ interface UserMessageProps {
 const UserMessage = ({ content }: UserMessageProps) => {
   return (
     <div className="flex justify-end pb-4 pr-2 pl-10">
-      <Card className="rounded-g bg-muted p-3 shadow-none border-none max-w-[80%] wrap-break-word">
+      <Card className="rounded-lg bg-muted p-3 shadow-none border-none max-w-[80%] wrap-break-word">
         {content}
       </Card>
     </div>
